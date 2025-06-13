23bc0a2134dc674044615c80c1269b1ad523513b
Jadhav-Nandini <nandinij780@gmail.com>
2025-06-14 03:27:56 +0530

    create user controller

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/user.js";
+import createToken from "../utils/createToken.js"
 
 const createUser = async(req,res) =>{
     const {username , email, password} = req.body;
@@ -9,6 +10,34 @@ const createUser = async(req,res) =>{
 
    const userExits = await User.findOne({email})
    if(userExits) res.status(400).send("User Already Exists");
+
+   const newUser = new User({username, email, password})
+
+   try{
+
+    await newUser.save();
+    createToken(res, newUser._id);
+    
+    res.status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        isAdmin: newUser.isAdmin
+    });
+
+   }
+   catch(error) {
+
+    console.error("register error", error.message);
+    res.status(400).json({
+        message: "Invalid user data" , error: error.message});
+    
+    
+    
+    // res.status(400)
+    // throw new Error("Invalid User Data")
+   }
+
 }
 
 export default createUser;
